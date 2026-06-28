8c229558c483d3f2557c3f34532332bb5379fd1f
MDC985 <mdchealthservices@gmail.com>
2026-06-28 17:00:36 +0100

    Remove base path from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/MDC-HEALTH-SERVICES---PORTF-LIO-/'
+  plugins: [react()]
 })
